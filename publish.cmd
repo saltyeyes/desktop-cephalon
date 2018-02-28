afb62dc284e876598f9bd33d51590ff479f347fb
Chris Lohmann <chris.lohmann@outlook.com>
2018-02-28 11:18:48 +0000

    Initial import

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=desktopcephalon-Web-Deploy.pubxml -p:Password=jp64AlDaJWKerx6fXpimz8bjcQC8GQ63oqJ7pw7ptkh6H8SPdepls5ZeK32W
+
